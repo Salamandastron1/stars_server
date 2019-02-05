9d9828376e483cb7984a5f3ad0b7ef88d85db173
Tim Garrity <tag.garrity@gmail.com>
2019-02-05 00:01:13 -0700

    set up parser

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const config = require('./knexfile')[environment];
 
 const database = knex(config);
 const app = express();
+
+app.use(express.json());
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`)
+});
+
+module.exports = app;
