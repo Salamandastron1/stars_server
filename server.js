acfd89d51ce8fce3c6caa680fbbc45560c4ac560
Tim Garrity <tag.garrity@gmail.com>
2019-02-05 23:15:22 -0700

    moved DB variables to model file

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const knex = require('knex');
 const cors = require('cors');
-const environment = process.env.NODE_ENV || 'development';
-const config = require('./knexfile')[environment];
-const database = knex(config);
 const app = express();
+
 const corsOptions = {
   origin: 'https://salamandastron1.github.io/hubble-bubble/',
   optionsSuccessStatus: 200,
