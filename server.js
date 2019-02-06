55ae721ab243c9dd9cf41923a094d89f8386d270
Tim Garrity <tag.garrity@gmail.com>
2019-02-05 23:01:08 -0700

    add i urlencoding for chars, start CRUD

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const app = express();
 const corsOptions = {
   origin: 'https://salamandastron1.github.io/hubble-bubble/',
   optionsSuccessStatus: 200,
-}
-
+};
 app.use(express.json());
-
-app.options('*', cors(corsOptions))
+app.use(express.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
+app.options('*', cors(corsOptions))
+
+app.get('/signin', )
+
 
 app.listen(app.get('port'), () => {
   console.log(`Listening on port ${app.get('port')}`)
