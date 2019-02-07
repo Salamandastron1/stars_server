ab5bb135b6352f072f6d5f6515a4d27ab1110f5f
Tim Garrity <tag.garrity@gmail.com>
2019-02-06 21:48:47 -0700

    initial logic for user controller using CRUD

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,12 @@
 const User = require('../models/User');
+
+exports.show = function(request, response) {
+  const { body } = request;
+  // code here for checking params
+  User.find()
+    .then({ stars, username} => response.status(200).json({
+      stars,
+      username,
+    }))
+    .catch(error => response.status(500).json({ error })) 
+}
