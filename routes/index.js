be2a63a29abe687b42748f500f0a22b7523e2303
Tim Garrity <tag.garrity@gmail.com>
2019-02-06 21:12:43 -0700

    settle on all routes and controller names

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const userController = require('./controllers/userController');
-const avatarController = require
+const avatarController = require('./controllers/avatarController');
 
-export default app => {
-  app.get('/users', userController.login)
-  app.post('/users', userController.signUp)
-  app.put('/users/stars', userController.updateStars)
-  app.put('/avatar/', userController.avatarChange)
-  app.delete('/deleteLogins', isController)
-}
+export default function(app) {
+  app.get('/users', userController.login);
+  app.post('/users', userController.signUp);
+  app.put('/users', userController.updateStars);
+  app.post('/avatar/', avatarController.addAvatar);
+  app.put('/avatar', avatarController.updateAvatar);
+  app.delete('/avatar', avatarController.deleteAvatar).
+};
