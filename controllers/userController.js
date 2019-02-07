7e21a1e8a3a9cb3d5455de838541ddbd61f3af1c
Tim Garrity <tag.garrity@gmail.com>
2019-02-06 21:53:35 -0700

    controller created for creating a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,11 @@ exports.show = function(request, response) {
     }))
     .catch(error => response.status(500).json({ error })) 
 }
+
+exports.create = function(request, response) {
+  const { body } = request;
+  //code here to check params
+  User.create()
+    .then(id => response.status(204).json({ id }))
+    .catch(error => response.status(500).json({ error }))
+}
