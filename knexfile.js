f7bff72e8c25200072da0462c1882bc841a926b3
Tim Garrity <tag.garrity@gmail.com>
2019-02-11 18:07:49 -0700

    set up knex file for different db environments

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: 'postgres://localhost/stars',
     migrations: {
       directory: './db/migrations',
@@ -13,4 +13,26 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/testStar',
+    migrations: {
+      direction: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/testing',
+    },
+    useNullAsDefault: true,
+  },
+  production: {
+    client: 'pg',
+    connection: "process.env.DATABASE_URL" + "ssl=true",
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/production',
+    },
+    useNullAsDefault: true,
+  }
 };
