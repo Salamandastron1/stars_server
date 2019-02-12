30d1c468c711a4972ec55eaae6b07b7cfb30045c
Tim Garrity <tag.garrity@gmail.com>
2019-02-11 17:19:53 -0700

    setup initial db and server connection w/ knex

--- knexfile.js
+++ knexfile.js
@@ -3,40 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/stars'
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: 'postgres://localhost/stars',
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
