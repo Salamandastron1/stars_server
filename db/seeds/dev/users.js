0ee6ac28e701a6a50e7cbd8b2b1a3d0e5bcb64e1
Tim Garrity <tag.garrity@gmail.com>
2019-02-12 21:05:00 -0700

    create custom seed data

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -1,14 +1,44 @@
+const userData = [
+  {
+    username: 'Tim',
+    email: 'timmybobjoe@gmail.com',
+    password: 'timmybob',
+  },
+  {
+    username: 'Drake',
+    email: 'drakeathon@yahoo.com',
+    password: 'fakeandgay',
+    stars: 50,
+  },
+  {
+    username: 'Dina',
+    email: 'dinaisbomb@aol.com',
+    password: 'thewiz',
+    stars: 20,
+  }
+];
+const avatar = [
+  'www.hellokitty.com/url?3',
+  'www.kittykatsrcool.org/users/api/v3/user=moot?moot',
+  'https//:localhost/3000'
+]
+
+function usersCreate(knex, users) {
+
+}
+
+function avatarsCreate(knex, avatars) {
+
+}
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('avatars').del()
     .then(() => knex('users'.del())
     .then(() =>
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      const userPromises = [];
+      
+      
+    );
     });
 };
