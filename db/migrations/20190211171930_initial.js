d39c11de5f674c9aec4bb42860db12cb9f4aded9
Tim Garrity <tag.garrity@gmail.com>
2019-02-12 20:02:05 -0700

    create schema for user table

--- db/migrations/20190211171930_initial.js
+++ db/migrations/20190211171930_initial.js
@@ -1,5 +1,12 @@
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('users', table => {
+      table.increments('id').primary();
+      table.string('username');
+      table.string('email').unique();
+      table.string('password');
+    }
+  ])
 };
 
 exports.down = function(knex, Promise) {
