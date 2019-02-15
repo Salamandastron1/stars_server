1d0e7b411f08768d68292fb8b94c6213e70b2b8b
Tim Garrity <tag.garrity@gmail.com>
2019-02-15 14:14:19 -0700

    adjust for lint errors

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -15,41 +15,42 @@ const userData = [
     email: 'dinaisbomb@aol.com',
     password: 'thewiz',
     stars: 20,
-  }
+  },
 ];
 const avatars = [
   'www.hellokitty.com/url?3',
   'www.kittykatsrcool.org/users/api/v3/user=moot?moot',
-  'https//:localhost/3000'
-]
+  'https//:localhost/3000',
+];
 
 function usersCreate(knex, user) {
-  const { username, email, password, stars } = user;
+  const {
+    username,
+    email,
+    password,
+    stars,
+  } = user;
 
-    return knex('users').insert({
-      username,
-      email,
-      password,
-      stars,
-    })
+  return knex('users').insert({
+    username,
+    email,
+    password,
+    stars,
+  });
 }
 
 function avatarsCreate(knex, url) {
-  return knex('avatars').insert({avatar_url: url})
+  return knex('avatars').insert({ avatar_url: url });
 }
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('avatars').del()
     .then(() => knex('users').del())
     .then(() => {
-      const userPromises = userData.map(user => {
-        return usersCreate(knex, user)
-      })
-      const avatarPromises = avatars.map(url => {
-        return avatarsCreate(knex, url)
-      })
-      
-      return Promise.all([...userPromises, ...avatarPromises])
-  });
+      const userPromises = userData.map(user => (usersCreate(knex, user)));
+      const avatarPromises = avatars.map(url => (avatarsCreate(knex, url)));
+
+      return Promise.all([...userPromises, ...avatarPromises]);
+    });
 };
